5f1555acaff3cc16fbceb6fc2b862854d79a3a57
Mufaddal Barwaniwala <invinciblemuffi@gmail.com>
2021-12-25 17:12:04 +0530

    first commit

--- README.md
+++ README.md
@@ -1,24 +1,13 @@
-# coach-finder
+# Coach/Mentor Finder App
 
-## Project setup
-```
-npm install
-```
+### Register as a coach
+### Contact a coach by sending a request
+### Coach can view his/her requests sent by students
+### Filter a coach based on expertise
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+### Tech Stack
+- VueJS 3.x
+- vuex
+- vue-router
 
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+Deployed on - 
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>Coach Finder</h2>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+};
 </script>
 
 <style>
-#app {
+* {
+  box-sizing: border-box;
+}
+
+html,
+body {
+  margin: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
