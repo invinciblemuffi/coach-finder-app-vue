7eeadf35e726373096fbb916b6d4ef18e5f32ffd
Mufaddal Barwaniwala <invinciblemuffi@gmail.com>
2021-12-28 20:49:25 +0530

    UI Components with slots, dynamic class, props

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,30 +1,34 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :id="coach.id"
-        :firstName="coach.firstName"
-        :lastName="coach.lastName"
-        :areas="coach.areas"
-        :rate="coach.hourlyRate"
-      ></coach-item>
-    </ul>
-    <h3 v-else>No Coaches found.</h3>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :firstName="coach.firstName"
+          :lastName="coach.lastName"
+          :areas="coach.areas"
+          :rate="coach.hourlyRate"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No Coaches found.</h3>
+    </base-card>
   </section>
 </template>
 
 <script>
 import CoachItem from "../../components/coaches/CoachItem.vue";
+import BaseButton from "../../components/ui/BaseButton.vue";
 export default {
   components: {
     CoachItem,
+    BaseButton,
   },
   computed: {
     filteredCoaches() {
