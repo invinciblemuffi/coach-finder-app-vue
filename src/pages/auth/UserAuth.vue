630962802b1d4593a3297a0182f02364996007e2
Mufaddal Barwaniwala <invinciblemuffi@gmail.com>
2022-01-01 20:38:45 +0530

    Authentication module with signup, error handling, modal working

---
+++ src/pages/auth/UserAuth.vue
@@ -0,0 +1,144 @@
+<template>
+  <div>
+    <base-modal
+      :show="!!hasError"
+      title="An error occured!"
+      @close="handleError"
+    >
+      <p>{{ hasError }}</p>
+    </base-modal>
+    <base-modal :show="isLoading" title="Authenticating..." fixed>
+      <div class="loaderContainer">
+        <loader :color="dotColor"></loader>
+      </div>
+    </base-modal>
+    <base-card>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">E-mail</label>
+          <input type="text" id="email" v-model.trim="email" />
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model.trim="password" />
+        </div>
+        <p v-if="!formIsValid">
+          Please enter a valid email and password. Password must be atleast 6
+          characters long.
+        </p>
+        <base-button>{{ submitBtnCaption }}</base-button>
+        <base-button type="button" mode="flat" @click="switchAuthMode">{{
+          switchModeBtnCaption
+        }}</base-button>
+      </form>
+    </base-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      formIsValid: true,
+      mode: "login",
+      isLoading: false,
+      hasError: null,
+    };
+  },
+  computed: {
+    dotColor() {
+      return "#3d008d";
+    },
+    submitBtnCaption() {
+      if (this.mode === "login") {
+        return "Login";
+      } else {
+        return "Signup";
+      }
+    },
+    switchModeBtnCaption() {
+      if (this.mode === "login") {
+        return "Signup instead";
+      } else {
+        return "Login instead";
+      }
+    },
+  },
+  methods: {
+    async submitForm() {
+      this.formIsValid = true;
+      if (
+        this.email === "" ||
+        !this.email.includes("@") ||
+        this.password.length < 6
+      ) {
+        this.formIsValid = false;
+        return;
+      }
+      this.isLoading = true;
+      try {
+        if (this.mode === "login") {
+          //   dispatch login action
+        } else {
+          await this.$store.dispatch("signUpAction", {
+            email: this.email,
+            password: this.password,
+          });
+        }
+      } catch (error) {
+        this.hasError = error.message || "Failed to signup.";
+      }
+
+      this.isLoading = false;
+    },
+    switchAuthMode() {
+      if (this.mode === "login") {
+        this.mode = "signup";
+      } else {
+        this.mode = "login";
+      }
+    },
+    handleError() {
+      this.hasError = null;
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  margin: 1rem;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus {
+  border-color: #3d008d;
+  background: #faf6ff;
+  outline: none;
+}
+
+.loaderContainer {
+  display: flex;
+  justify-content: center;
+  margin: 2rem auto;
+}
+</style>
