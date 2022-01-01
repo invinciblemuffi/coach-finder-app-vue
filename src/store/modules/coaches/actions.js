21e2939ed6fb744caa54aedac6cf7df9d9926e2e
Mufaddal Barwaniwala <invinciblemuffi@gmail.com>
2022-01-01 12:13:57 +0530

    Put and Get coaches form firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,7 +1,7 @@
 export default {
-  registerCoachAction(context, data) {
+  async registerCoachAction(context, data) {
+    const userId = context.rootGetters.userId;
     const newCoachData = {
-      id: context.rootGetters.userId,
       firstName: data.fName,
       lastName: data.lName,
       description: data.desc,
@@ -9,6 +9,49 @@ export default {
       areas: data.areas,
     };
 
-    context.commit("registerCoachMutation", newCoachData);
+    const resp = await fetch(
+      `https://coach-finder-de40b-default-rtdb.asia-southeast1.firebasedatabase.app/coaches/${userId}.json`,
+      {
+        method: "PUT",
+        body: JSON.stringify(newCoachData),
+      }
+    );
+
+    const respData = await resp.json();
+    console.dir(respData);
+
+    if (!resp.ok) {
+      // error
+    }
+
+    context.commit("registerCoachMutation", { ...newCoachData, id: userId });
+  },
+
+  async loadAllCoachesAction(context) {
+    const resp = await fetch(
+      `https://coach-finder-de40b-default-rtdb.asia-southeast1.firebasedatabase.app/coaches.json`
+    );
+
+    const respData = await resp.json();
+    console.dir(respData);
+
+    if (!resp.ok) {
+      // error
+    }
+
+    const coaches = [];
+    for (let key in respData) {
+      const coach = {
+        id: key,
+        firstName: respData[key].firstName,
+        lastName: respData[key].lastName,
+        description: respData[key].description,
+        hourlyRate: respData[key].hourlyRate,
+        areas: respData[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    context.commit("getAllCoachesMutation", coaches);
   },
 };
--- src/store/modules/coaches/mutations.js
+++ src/store/modules/coaches/mutations.js
@@ -2,4 +2,8 @@ export default {
   registerCoachMutation(state, payload) {
     state.coaches.unshift(payload);
   },
+
+  getAllCoachesMutation(state, payload) {
+    state.coaches = payload;
+  },
 };
